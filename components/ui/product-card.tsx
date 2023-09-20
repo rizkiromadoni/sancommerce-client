d198390d907d18f61fc46a47783ff909c93c18c6
rizkiromadoni <coradex06@gmail.com>
2023-09-20 23:04:58 +0700

    feat: adding individual product page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -4,8 +4,9 @@ import { Product } from '@/types';
 import Image from 'next/image';
 import React from 'react'
 import IconButton from './icon-button';
-import { Expand, ShoppingCart } from 'lucide-react';
+import { Expand, Router, ShoppingCart } from 'lucide-react';
 import Currency from './currency';
+import { useRouter } from 'next/navigation';
 
 interface ProductCardProps {
   data: Product
@@ -14,8 +15,14 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  }
+
   return (
-    <div className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
+    <div onClick={handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
       <div className='aspect-square rounded-xl relative bg-gray-100'>
         <Image src={data?.images?.[0]?.url} alt='Image' fill className='aspect-square object-cover rounded-md' />
         <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
